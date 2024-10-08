bdf1e574f97d4292c45e25177014ec2edf00e7ea
Yui-geol <gamoon777@naver.com>
2024-10-08 20:39:56 +0900

    feat: 폼 데이터를 DTO로 받는 기능 추가

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -3,4 +3,18 @@ package com.example.firstproject.dto;
 public class ArticleForm {
     private String title;
     private String content;
+
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+
+    public ArticleForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
 }
