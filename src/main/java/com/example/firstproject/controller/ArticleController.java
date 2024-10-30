bcc7ffc50cd1e0fbf25809ada8a5f6e6e810ce3a
Yui-geol <gamoon777@naver.com>
2024-10-30 15:40:21 +0900

    feat: h2 데이터 읽기 추가

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,8 +6,13 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j //로깅 기능을 위한 어노테이션
 @Controller
@@ -23,7 +28,7 @@ public class ArticleController {
     //
     @PostMapping("articles/create")
     public String createArticle(ArticleForm form) {
-        
+
         log.info(form.toString()); //아래의 로그를 대체
         //System.out.println(form.toString());
 
@@ -35,6 +40,27 @@ public class ArticleController {
 
         // 2. 리포지터리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);
+        log.info(saved.toString());
         return "articles/new";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " + id);
+        //1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+        return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model) {
+        //1. 모든 데이터 가져오기
+        List<Article> articleEntityList = articleRepository.findAll();
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("articleList", articleEntityList);
+        //3. 뷰 페이지 설정하기
+        return "articles/index";
     }
 }
